8eb5c44602dd3296597d9651727cb9d58db19ee5
Muzafar <makestar99@gmail.com>
2024-12-02 16:18:25 +0500

    learning node and github

---
+++ 0-modules.js
@@ -0,0 +1,9 @@
+const names = require('./1-names')
+const sayHi = require('./2-utils')
+const data = require('./3-alternative-export')
+require('./4-mind-grenade')
+
+sayHi('susan');
+sayHi(jeremy);
+sayHi(names.andrew);
+sayHi(names.make);
---
+++ 1-names.js
@@ -0,0 +1,9 @@
+// local
+const secret = 'super secret'
+// share
+const john = 'john'
+const andrew = 'andrew'
+const make = 'make'
+const jeremy = 'jeremy'
+
+module.exports = {john, andrew, make}
